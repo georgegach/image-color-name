d7ce84853b9b85e0d3fe599d832ccbd5f3a20189
George Gach <georgeggach@gmail.com>
2018-10-30 22:11:53 -0400

    cleanup

--- js/script.js
+++ js/script.js
@@ -132,8 +132,8 @@ window.onload = function () {
         ctx.arc(cross + pad, cross + pad, cross / 3, 0, 2 * Math.PI, false);
         ctx.stroke();
 
-        document.getElementById('canvas').style.cursor = 'crosshair';
-        document.getElementById('canvas').style.cursor = 'url(' + cursor.toDataURL() + ') ' + (pad + cross) + ' ' + (pad + cross) + ' , crosshair';
+        canvas.style.cursor = 'crosshair';
+        canvas.style.cursor = 'url(' + cursor.toDataURL() + ') ' + (pad + cross) + ' ' + (pad + cross) + ' , crosshair';
     }
 
 
